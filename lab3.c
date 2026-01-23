2c39adaa1ebcdaea2393efbce2d3f800bc7c800f
MrFree31 <kon.eremeev@gmail.com>
2026-01-23 23:29:37 +1000

    Третья лаба, первый подход

---
+++ lab3.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <windows.h> //Кодировка русского языка
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+int main(){
+SetConsoleOutputCP(CP_UTF8); //Кодировка русского языка
+printf("1)Замена строчных\n");
+printf("Введите строку(до 80 значений на английском)\n");
+char str[81];
+fgets(str,sizeof(str),stdin);
+for(int i=0;i<strlen(str);++i)
+    if(str[i]=='a'){
+        str[i]='A';
+    }
+
+    else if(str[i]=='b'){
+        str[i]='B';
+    }
+printf("===Вывод===");
+for(int i=0;i<strlen(str);++i){
+    printf("%c",str[i]);
+    }
+
+printf("2)Магический квадрат\n");
+print("Порядок матрицы\n");
+scanf("%d",p);
+int **mat = (int**)malloc(p * sizeof(int*));
+for(int i=0;i<3;++i)
+    mat[i] = (int*)malloc(p * sizeof(int));
+
+printf("Введите переменные(9 шт)\n");
+for(int i=0;i<3;++i){
+    for(int j=0;j<3;++j){
+    scanf("%d",&mat[i][j]);
+    }
+}
+
+int sg = 0,sp = 0;
+for(int i=0;i<p;++i)
+    sg += mat[i][i], sp += mat[i][3-1-i];
+    for(int j=0;;)
+
+if(sg==sp){
+    printf("Наша матрица - Магический квадрат, Диагонали равны:%d",sg);
+}
+
+else{
+printf("Наша матрица - никакой не магический квадрат. Главная диагональ - %d, побочная - %d",sg,sp);
+}
+
+return 0;
+}
