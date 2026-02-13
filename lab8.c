80da5136ebbb733cb16cb957ecfa56bd4b9eba0d
MrFree31 <kon.eremeev@gmail.com>
2026-02-13 23:44:00 +1000

    Восьмая лабораторная. Второй подход.

--- lab8.c
+++ lab8.c
@@ -10,21 +10,47 @@ struct person{
 int main(){
     SetConsoleOutputCP(CP_UTF8);
     FILE *f = fopen("lab8.txt","r");
-    struct person p[100];
-    int i = 0;
-    while(fscanf(f, "%19s %19s %s %d %f", 
-        p[i].name, p[i].surname,p[i].gender,p[i].birthyear,p[i].height)==5){
-            i+=1;
-            if(i>=100) break;
+    struct person *p = NULL;
+    int c = 0;
+    while(1){
+        struct person temp;
+
+        if (fscanf(f, "%19s %19s %c %d %f", 
+        temp.name, temp.surname,&temp.gender,&temp.birthyear,&temp.height)!=5){
+            break;
+        }
+        struct person *np = realloc(p,(c+1)*sizeof(struct person));
+        if(np==NULL){
+            printf("Ошибка: С выделением памяти напартачили!😡\n");
+            free(p);
+            return 1;
+        }
+        p = np;
+        p[c]=temp;
+        c+=1;
+    }
+    struct person pp[c];
+    for(int i=0;i<c;i++){
+        pp[i]=p[i];
     }
     fclose(f);
     char poryad;
-    printf("По какому критерию сравнить челиков?(Введи первую букву:И,Ф,Д,П,Р): \n");
-    scanf('%s',&poryad);
-    if(poryad=='И'){}
-    else if(poryad=='Ф'){}
-    else if(poryad=='Д'){}
-    else if(poryad=='П'){}
-    else if(poryad=='Р'){}
+    printf("По какому критерию сравнить челиков?(N,S,B,G,H): \n");
+    scanf(" %c",&poryad);
+    if(poryad=='N'){}
+    else if(poryad=='S'){}
+    else if(poryad=='B'){
+        for(int i=0; i<c-1; i++){
+            for(int j = i+1; j<c; j++){
+                if(pp[i].birthyear>pp[j].birthyear){
+                    //struct person temp = pp[i];
+                    pp[i]=pp[j];
+                    pp[j]=temp;
+                }
+            }
+        }
+    }
+    else if(poryad=='G'){}
+    else if(poryad=='H'){}
     else{printf("БУКВЫ ИЗ ПЕРЕЧНЯ ВВЕДИ💀");}
 }
