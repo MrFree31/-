6be2ff855ba5349d61a6b32996c8012f8a3988e8
MrFree31 <kon.eremeev@gmail.com>
2026-03-14 19:18:57 +1000

    ОАП Лабораторная 3. 2подход.

--- ОАП/LAB3_ОАП.c
+++ ОАП/LAB3_ОАП.c
@@ -5,8 +5,8 @@
 #include <windows.h>
 
 struct tnode{
-    int data;
-    char *word;
+    int count;
+    char *w;
     struct tnode *lchild;
     struct tnode *rchild;
 };
@@ -35,7 +35,51 @@ int slov(FILE *fx, char *word, int max_len){
     return i;
 }
     
-int main(){
+char *zam(const char *str){
+	if(str == NULL) return NULL;
+	char *new_str = (char*)malloc(strlen(str)+1);//Один дополнительный для нуля-терминатора
+	if(new_str!=NULL){
+		strcpy(new_str,str);
+	}
+	return new_str;
+}
+
+struct tnode *new_child(const char *word){
+	struct tnode *kor = (struct tnode*)malloc(sizeof(struct tnode));
+	if(kor == NULL){
+		printf("Ошибка! Память новому узлу не выделилась (new_child)");
+		return 1;
+	}
+	kor->w = zam(word);
+	if(kor->w == NULL){
+		printf("Ошибка! Память слову не выделилась (strcopy)");
+	}
+	kor->count = 1;
+	kor->lchild=kor->rchild=1;
+	return kor;
+}
+
+ struct tnode *vstavka(struct tnode *kor, const char *word){
+	if(kor == NULL){
+		return new_child(word);
+	}
+	int cmp = strcmp(word,kor->w);
+	if(cmp < 0) kor->lchild = vstavka(kor->lchild, word);
+	else if(cmp >0) kor->rchild = vstavka(kor->rchild, word);
+	else kor->count++;
+	
+	return kor;
+}
+
+void tree_output(FILE *fo, const struct tnode *kor){
+	if(kor != NULL){
+		tree_output(fo, kor->lchild);
+		fprintf(fo, "Слово %s встречаются %d раз",kor->w,kor->count);
+		tree_output(fo, kor->rchild);
+	}
+}
+
+int main(argc,argv){
     SetConsoleOutputCP(CP_UTF8);
     int MAX_LEN = 50;
     char input[25];
@@ -43,32 +87,36 @@ int main(){
     int len;
     int ks = 0;
     struct tnode tree;
+printf("Введите имя файла: ");
 
-    printf("Введите имя файла: ");
-    if(fgets(input,sizeof(input),stdin)==NULL){
-        printf("Ошибка! Введена пустая строка");
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+        printf("Ошибка ввода.\n");
         return 1;
     }
 
-    input[strcspn(input,"\n")]='\0';
-    FILE *fx;
-    fx = fopen(input,"r");
-    if(fx==NULL){
-        printf("Ошибка! Файл не открыт");
+    input[strcspn(input, "\n")] = '\0';
+
+    FILE *f1;
+    f1=fopen(input,"r");
+    if(f1 == NULL){
+        printf("Ошибка! Файл %s не прочитан - ПРОПУСКАЕМ \n",input);
         return 1;
     }
-//Отладка slov
-    while((len=slov(fx,word,MAX_LEN))>0){
-        ks++;
-        printf("Слово №%d - '%s', длинна = %d\n",ks,word,len);
-    }
 
-    if(len == 0 && feof(fx)){
-        printf("Кол-во слов: %d\n",ks);
-    }
-    else{
-        printf("Ошибка!");
-    }
-    fclose(fx);
+    printf("Файл %s открыт\n", input);
     return 0;
 }
+//     while((len=slov(fx,word,MAX_LEN))>0){
+//         ks++;
+//         printf("Слово №%d - '%s', длинна = %d\n",ks,word,len);
+//     }
+
+//     if(len == 0 && feof(fx)){
+//         printf("Кол-во слов: %d\n",ks);
+//     }
+//     else{
+//         printf("Ошибка!");
+//     }
+//     fclose(fx);
+//     return 0;
+// }
